897940f332fc0b4dfc0e9e291fb292ef61c450c2
Gago31 <gago3110@gmail.com>
2026-07-20 13:43:16 -0400

    Export optimization

--- editor/tree_preprocessor.gd
+++ editor/tree_preprocessor.gd
@@ -7,6 +7,9 @@ var video_files: Array[String] = []
 
 
 func collect_resources(root: ClassRoot) -> UsedResources:
+	audio_files.clear()
+	image_files.clear()
+	video_files.clear()
 	_collect_resources_from_node(root)
 	var resources := UsedResources.new(audio_files, image_files, video_files)
 	return resources
@@ -21,11 +24,17 @@ func _collect_resources_from_node(node: ClassNode) -> void:
 func _process_leaf_node(node: ClassLeaf) -> void:
 	var entity := node.entity
 	if entity is AudioEntity:
-		audio_files.append((entity as AudioEntity).audio_path)
+		var path := (entity as AudioEntity).audio_path
+		if path not in audio_files:
+			audio_files.append(path)
 	elif entity is ImageEntity:
-		image_files.append((entity as ImageEntity).image_path)
+		var path := (entity as ImageEntity).image_path
+		if path not in image_files:
+			image_files.append(path)
 	elif entity is VideoEntity:
-		video_files.append((entity as VideoEntity).video_path)
+		var path := (entity as VideoEntity).video_path
+		if path not in video_files:
+			video_files.append(path)
 
 class UsedResources:
 	var audio: Array[String]
